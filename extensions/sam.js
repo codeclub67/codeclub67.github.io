bfcf958238ac744c58744ec5cebf933a34cc8f5d
codeclub67 <114946583+codeclub67@users.noreply.github.com>
2024-04-02 09:23:34 +0100

    Update sam.js

--- extensions/sam.js
+++ extensions/sam.js
@@ -1,12 +1,11 @@
 import StartAudioContext from 'startaudiocontext';
-import bowser from 'bowser';
+//import bowser from 'bowser';
 
-let AUDIO_CONTEXT;
-if (!bowser.msie) {
-    AUDIO_CONTEXT = new (window.AudioContext || window.webkitAudioContext)();
-
-    StartAudioContext(AUDIO_CONTEXT);
-}
+//let AUDIO_CONTEXT;
+//if (!bowser.msie) {
+AUDIO_CONTEXT = new (window.AudioContext)();
+StartAudioContext(AUDIO_CONTEXT);
+//}
 
 /**
  * Wrap browser AudioContext because we shouldn't create more than one
