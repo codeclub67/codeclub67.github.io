01e7b84c4575e175a430aeca61a9dde60853a393
codeclub67 <114946583+codeclub67@users.noreply.github.com>
2024-04-02 09:53:05 +0100

    Update sam.js

--- extensions/sam.js
+++ extensions/sam.js
@@ -7,6 +7,8 @@
 //StartAudioContext(AUDIO_CONTEXT);
 //}
 
+audioContext = window.AudioContext || window.webkitAudioContext;
+
 /**
  * Wrap browser AudioContext because we shouldn't create more than one
  * @return {AudioContext} The singleton AudioContext
