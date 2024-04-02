e4add9a185e66a4379748d53f6beb4088fa791e4
codeclub67 <114946583+codeclub67@users.noreply.github.com>
2024-04-02 09:12:02 +0100

    Update sam.js

--- extensions/sam.js
+++ extensions/sam.js
@@ -1,3 +1,21 @@
+import StartAudioContext from 'startaudiocontext';
+import bowser from 'bowser';
+
+let AUDIO_CONTEXT;
+if (!bowser.msie) {
+    AUDIO_CONTEXT = new (window.AudioContext || window.webkitAudioContext)();
+
+    StartAudioContext(AUDIO_CONTEXT);
+}
+
+/**
+ * Wrap browser AudioContext because we shouldn't create more than one
+ * @return {AudioContext} The singleton AudioContext
+ */
+export default function () {
+    return AUDIO_CONTEXT;
+}
+
 /**
  * This is SamJs.js v0.2.0
  *
